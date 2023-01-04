27f314ae154d407318cbea35fcb1afb1cc7cfbbf
ShadePrima <aleksandrag1984@gmail.com>
2023-01-04 19:23:50 +0200

    scroll to top

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import Contacts from './pages/Contacts';
 import ProjectPage from './pages/ProjectPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Projects from './components/Projects/Projects';
+import ScrollToTop from './components/utils/scrollToTop';
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <ScrollToTop />
         <Navbar />
         <Routes>
-          <Route path='/' element={<Projects />} />
+          <Route path='/' element={<MyProjects />} />
           <Route path='/project' element={<ProjectPage />} />
           <Route path='/skills' element={<Skills />} />
-          <Route path='/contacts' element={<Contacts />} />
+          <Route path='/contacts' element={<Contacts acts />} />
         </Routes>
         <Footer />
       </Router>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,6 +5,9 @@ import sun from '../../img/icons/sun.svg';
 import moon from '../../img/icons/moon.svg';
 
 const Navbar = () => {
+  const activeLink = 'nav-list__link nav-list__link--active';
+  const normalLink = 'nav-list__link';
+
   return (
     <nav className='nav'>
       <div className='container'>
@@ -20,17 +23,34 @@ const Navbar = () => {
 
           <ul className='nav-list'>
             <li className='nav-list__item'>
-              <NavLink to='/' className='nav-list__link nav-list__link--active'>
+              <NavLink
+                to='/'
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink
+                }
+              >
                 Projects
               </NavLink>
             </li>
+
             <li className='nav-list__item'>
-              <NavLink to='/skills' className='nav-list__link'>
+              <NavLink
+                to='/skills'
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink
+                }
+              >
                 Skills
               </NavLink>
             </li>
+
             <li className='nav-list__item'>
-              <NavLink to='/contacts' className='nav-list__link'>
+              <NavLink
+                to='/contacts'
+                className={({ isActive }) =>
+                  isActive ? activeLink : normalLink
+                }
+              >
                 Contacts
               </NavLink>
             </li>
